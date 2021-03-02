e6194a0af72ccad176375c2859bca69b1e4d8350
Orhan Albayati <70097003+Warchiefen@users.noreply.github.com>
2021-03-02 10:34:02 +0100

    Added values
    Added Bean sorts,
    Added Ingredients,
    Added CupType.

--- BaristaApi/Beverage.cs
+++ BaristaApi/Beverage.cs
@@ -1,12 +1,16 @@
 using System.Collections.Generic;
-
-
 //INGREDIENTS
 //ESPRESSO
 //MILK
 //MILK FOAM
 //CHOCOLATE SYRUP
 //WATER
+
+//CUPTYPE,
+//Small
+//Medium
+//Big
+
 public interface IBeverage{
 	List<string> Ingredients { get; }
     string CupType { get; }
@@ -14,43 +18,43 @@ public interface IBeverage{
 
 class Espresso : IBeverage
 {
-    public List<string> Ingredients => throw new System.NotImplementedException();
-    //Espresso 4/4 (span?)
-    public string CupType => throw new System.NotImplementedException();
+    public List<string> Ingredients => new List<string> { "Beans", "Water" };
+    //Espresso 4/4 / Water? (span?)
+    public string CupType => "Small";
     
 }
 
 class Latte : IBeverage
 {
-    public List<string> Ingredients => throw new System.NotImplementedException();
+    public List<string> Ingredients => new List<string> { "Beans", "Milk" };
     // Espresso 1/4, Milk 3/4
-    public string CupType => throw new System.NotImplementedException();
+    public string CupType => "Large";
 }
 
 class Cappucino : IBeverage
 {
-    public List<string> Ingredients => throw new System.NotImplementedException();
+    public List<string> Ingredients => new List<string> { "Beans", "Milk" , "Milk Foam"};
     // Espresso 1/4, Milk 1/4, Milk Foam 2/4
-    public string CupType => throw new System.NotImplementedException();
+    public string CupType => "Medium";
 }
 
 class Americano : IBeverages
 {
-    public List<string> Ingredients => throw new System.NotImplementedException();
+    public List<string> Ingredients => new List<string> { "Beans", "Water" };
     //Espresso 2/4, Water 2/4 
-    public string CupType => throw new System.NotImplementedException();
+    public string CupType => "Small";
 }
 
 class Macchiato : IBeverage
 {
-    public List<string> Ingredients => throw new System.NotImplementedException();
+    public List<string> Ingredients => new List<string> { "Beans", "Milk Foam" };
     // Espresso 1/4, Milk foam 3/4
-    public string CupType => throw new System.NotImplementedException();
+    public string CupType => "Medium";
 }
 
 class Mocha : IBeverage
 {
-    public List<string> Ingredients => throw new System.NotImplementedException();
+    public List<string> Ingredients => new List<string> { "Beans", "Chocolate Syrup", "Milk" };
     // Espresso 1/4, Chocholate syrup 1/4, Milk 2/4
-    public string CupType => throw new System.NotImplementedException();
+    public string CupType => "Large";
 }
