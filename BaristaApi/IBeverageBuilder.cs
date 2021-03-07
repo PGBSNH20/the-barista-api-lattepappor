49bd0a90f3b0096eb676b1d8649c29a69be99d55
Jonas Lindell <70151761+jonaslindell-th@users.noreply.github.com>
2021-03-07 20:44:08 +0100

    Moved interfaces to same file

---
+++ BaristaApi/IBeverageBuilder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaristaApi.Interface
+{
+    public interface IBeverageBuilderCoffeeType
+    {
+        IBeverageBuilderCupType CoffeeType(BeverageTypes.BeverageType type);
+    }
+    public interface IBeverageBuilderCupType
+    {
+        IBeverageBuilderBeanType SelectCup(CupTypes.CupType cupType);
+    }
+    public interface IBeverageBuilderBeanType
+    {
+        IBeverageBuilderBeanGrind ChooseBeans(BeanTypes.BeanType bean);
+    }
+    public interface IBeverageBuilderBeanGrind
+    {
+        IBeverageBuilderBoilWater GrindBean();
+    }
+    public interface IBeverageBuilderBoilWater
+    {
+        IBeverageBuilderIngredient BoilWater();
+    }
+    public interface IBeverageBuilderIngredient
+    {
+        IBeverageBuilderFinish AddIngredient(int milk = 0, int milkFoam = 0, int chocolateSyrup = 0);
+    }
+    public interface IBeverageBuilderFinish
+    {
+        Espresso ToBeverage();
+    }
+}
